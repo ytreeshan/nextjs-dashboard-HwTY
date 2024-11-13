f29fd4201f15847ca5eca1fd80dc7f751fa1c224
ytreeshan <tjainarine@gmail.com>
2024-11-13 04:19:04 -0500

    added auth.config.ts added auth.ts added middleware.ts ``` fix some eorrors fingers cross again

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import { sql } from '@vercel/postgres';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
+ 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -113,5 +116,23 @@ export async function deleteInvoice(id: string) {
       return { message: 'Deleted Invoice.' };
     } catch {
       return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+  }
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
     }
   }
