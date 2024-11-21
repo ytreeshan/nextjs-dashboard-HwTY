0fba4b879f4149aadccb1a8080f741ff13ee17fb
ytreeshan <44382924+ytreeshan@users.noreply.github.com>
2024-11-21 05:50:40 +0000

    config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
- 
+  // Enabling strict mode for React
+  reactStrictMode: true,
 
+  // Setting up environment variables
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
+
+
+  // Custom webpack configuration
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Fixes npm packages that depend on `fs` module
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
